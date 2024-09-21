fc53aacb4eda3de160ccd9b94538bd1c455ef348
Faisal Hanif <mehrfaisal111@example.com>
2024-09-21 10:56:32 -0700

    update status

--- src/components/Status.js
+++ src/components/Status.js
@@ -5,7 +5,7 @@ function Status(props) {
   <div>
   {props.visi=="visible" ?  
    <div className="alert alert-warning" role="alert">
-    Try reloading the page inside the topic to remove any discrepancy
+    Try reloading the page inside the topic to remove any discrepance
         </div> : <div className="h-4 bg-transparent border-transparent " role="alert"></div>
 }
   
